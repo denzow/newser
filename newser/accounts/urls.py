4f3ba5403f9e0d034b9c784e0236b9e841fad916
denzow <denzow@gmail.com>
2017-07-11 00:05:16 +0900

    MDL start
    I start to use material design light.
    list page is mock.

--- newser/accounts/urls.py
+++ newser/accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^profile$', views.index, name='profile'),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name='logout')
 ]
