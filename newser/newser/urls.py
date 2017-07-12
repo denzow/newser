0317b56800aa5228ea15b18ad466d6cfb1a04089
denzow <denzow@gmail.com>
2017-07-13 00:23:38 +0900

    add static url

--- newser/newser/urls.py
+++ newser/newser/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+
 urlpatterns = [
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^viewer/', include('viewer.urls', namespace='viewer')),
