ab4aa117a72e9b0f4aec8027158e9dc906c13800
denzow <denzow@gmail.com>
2017-07-30 23:37:57 +0900

    Pagination add
    pagination via django rest framework
    and transparent add article implement.

--- newser/backend/models.py
+++ newser/backend/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils.timezone import now, utc
-
-
+from django.conf import settings
 
 class RssFeeds(models.Model):
+    """
+    RSS Feeds
+    """
 
     url = models.URLField(max_length=2000)
     name = models.CharField(max_length=1000)
@@ -21,10 +23,17 @@ class RssFeeds(models.Model):
         return cls.objects.filter(valid=True)
 
     def get_max_timestamp(self):
+        """
+        関連フィードの最大の更新時間
+        :return:
+        """
         return self.related_articles.aggregate(models.Max('timestamp'))["timestamp__max"]
 
 
 class Articles(models.Model):
+    """
+    記事
+    """
 
     url = models.URLField(max_length=2000, primary_key=True)
     # base64
@@ -35,5 +44,14 @@ class Articles(models.Model):
     source = models.ForeignKey(RssFeeds, name="source", related_name="related_articles", default=None, null=True)
 
     def __str__(self):
-        return "{}".format(self.title)
+        return "({}){}".format(self.source, self.title)
+
+
+class ReadHistories(models.Model):
+    """
+    読み込み履歴
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="red_article", on_delete=models.CASCADE,)
+    article = models.ForeignKey(Articles, name="red_article")
+    timestamp = models.DateTimeField(default=now)
 
--- newser/newser/settings.py
+++ newser/newser/settings.py
@@ -56,7 +56,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 15
 }
 
 MIDDLEWARE = [
